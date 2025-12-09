e336b744b9699a27c19aba6fb9c9198227aab001
kaguya07 <93606330+kod-07@users.noreply.github.com>
2025-12-10 02:50:59 +0530

    Switch to relative paths and add .nojekyll

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mcq-platform/', // THIS MUST MATCH YOUR GITHUB REPO NAME
+  base: './', // Use relative paths
 })
